87756636620dab72927578d844d21bd8368b1c05
eightnoteight <mr.eightnoteight@gmail.com>
2014-10-30 22:17:47 +0530

    setup.py initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='spojbot',
+    version='0.1d',
+    description='Spoj Solution Submitter',
+    long_description='An Experiment on submitting solutions to spoj through a bot',
+    author='Srinivas Devaki',
+    author_email='mr.eightnoteight@gmail.com',
+    url='https://github.com/eightnoteight/spojbot',
+    packages=[
+        'spojbot'
+    ],
+    platforms=[
+        'linux'
+    ],
+    license='apache',
+    zip_safe=False)
