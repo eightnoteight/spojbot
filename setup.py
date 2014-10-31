98e8e92be962570f2615d6e6e0193142cf759e91
eightnoteight <mr.eightnoteight@gmail.com>
2014-10-31 20:10:05 +0530

    requirements and alpha release

--- setup.py
+++ setup.py
@@ -4,12 +4,16 @@ from setuptools import setup
 
 setup(
     name='spojbot',
-    version='0.1d',
+    version='0.1a',
     description='Spoj Solution Submitter',
     long_description='An Experiment on submitting solutions to spoj through a bot',
     author='Srinivas Devaki',
     author_email='mr.eightnoteight@gmail.com',
     url='https://github.com/eightnoteight/spojbot',
+    install_requires=[
+        "requests>=2.3.0",
+        "beautifulsoup4>=4.3.2"
+    ],
     packages=[
         'spojbot'
     ],
